7495854f43480b4f3d67cad85336400408106325
PFA16SCM60D <pdeenumsetti@hawk.iit.edu>
2017-02-09 09:04:34 -0600

    User Interface Changes

--- app/src/main/java/com/example/dpran/ilovezappos/MainActivity.java
+++ app/src/main/java/com/example/dpran/ilovezappos/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.dpran.ilovezappos;
 
+import android.content.Context;
 import android.databinding.DataBindingUtil;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.dpran.ilovezappos.R;
 import com.example.dpran.ilovezappos.databinding.ActivityMainBinding;
@@ -36,6 +41,27 @@ public class MainActivity extends AppCompatActivity {
                 model.getUser(binding.username.getText().toString());
             }
         });
+        ((EditText)findViewById(R.id.username)).setOnEditorActionListener(
+                new EditText.OnEditorActionListener() {
+                    @Override
+                    public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                        // Identifier of the action. This will be either the identifier you supplied,
+                        // or EditorInfo.IME_NULL if being called due to the enter key being pressed.
+                        if (actionId == EditorInfo.IME_ACTION_SEARCH
+                                || actionId == EditorInfo.IME_ACTION_DONE
+                                || event.getAction() == KeyEvent.ACTION_DOWN
+                                && event.getKeyCode() == KeyEvent.KEYCODE_ENTER) {
+                            viewModel.setImageView(view);
+                            viewModel.setPb(true);
+                            model.getUser(binding.username.getText().toString());
+                            InputMethodManager imm = (InputMethodManager) v.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+                            imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                            return true;
+                        }
+                        // Return true if you have consumed the action, else false.
+                        return false;
+                    }
+                });
     }
 
 }
